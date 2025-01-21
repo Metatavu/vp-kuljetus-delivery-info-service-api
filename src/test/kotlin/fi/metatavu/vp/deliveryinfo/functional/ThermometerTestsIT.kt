44ce5bf297e3d8b2f9c816133f00a90e1e88514f
Simeon Platonov <simeon.platonov@gmail.com>
2025-01-21 18:19:40 +0200

    Unused import

--- src/test/kotlin/fi/metatavu/vp/deliveryinfo/functional/ThermometerTestsIT.kt
+++ src/test/kotlin/fi/metatavu/vp/deliveryinfo/functional/ThermometerTestsIT.kt
@@ -9,7 +9,6 @@ import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.junit.TestProfile
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import java.lang.Thread.sleep
 import java.time.Instant
 import java.util.*
 
